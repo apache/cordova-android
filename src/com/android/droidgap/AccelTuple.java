91d93deabb864b9d29940cc4ebd212dc0a0238a5
Joe Bowser <bowserj@joe-bowsers-macbook.local>
2008-12-18 15:13:30 -0800

    Removing old code

--- src/com/android/droidgap/AccelTuple.java
+++
@@ -1,7 +0,0 @@
-package com.android.droidgap;
-
-public class AccelTuple {
-	public long accelX;
-	public long accelY;
-	public long accelZ;
-}
--- src/com/android/droidgap/GeoTuple.java
+++
@@ -1,7 +0,0 @@
-package com.android.droidgap;
-
-public class GeoTuple {
-	public double lat;
-	public double lng;
-	public double ele;
-}
