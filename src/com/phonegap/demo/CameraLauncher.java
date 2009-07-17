32cc9492a6d77051de9db7a350d72102150023c4
Joe Bowser <bowserj@volti.(none)>
2009-07-17 14:06:49 -0700

    Commiting the Launcher stub

---
+++ src/com/phonegap/demo/CameraLauncher.java
@@ -0,0 +1,19 @@
+package com.phonegap.demo;
+
+import android.content.Context;
+import android.webkit.WebView;
+
+
+
+public class CameraLauncher {
+
+	private WebView mAppView;
+	private Context mCtx;
+	
+	CameraLauncher(WebView view, Context ctx)
+	{
+		mAppView = view;
+		mCtx = ctx;
+	}
+	
+}
