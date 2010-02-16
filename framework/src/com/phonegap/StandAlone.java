3d91a98b3a55360aeac1a3d2428ccaa207adec7c
Brock Whitten <brock@sintaxi.com>
2010-02-16 12:11:43 -0800

    Fixing up StandAlone for debugging purposes

--- framework/src/com/phonegap/StandAlone.java
+++ framework/src/com/phonegap/StandAlone.java
@@ -11,22 +11,7 @@ public class StandAlone extends DroidGap {
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        /* Load a URI from the strings.xml file */
-        Class<R.string> c = R.string.class;
-        Field f;
-        String uri;
-        
-        int i = 0;
-        
-        try {
-          f = c.getField("url");
-          i = f.getInt(f);
-          uri = this.getResources().getString(i);
-        } catch (Exception e)
-        {
-          uri = "http://www.phonegap.com";
-        }
-        super.loadUrl(uri);                        
+        super.loadUrl("file:///android_asset/www/index.html");                        
     }		
 	
 }
