f848527c288b0063bdc4e250eb2d11709d266b4a
macdonst <simon.macdonald@gmail.com>
2011-02-03 09:45:20 +0800

    Upping version to 0.9.4

--- framework/src/com/phonegap/Device.java
+++ framework/src/com/phonegap/Device.java
@@ -20,7 +20,7 @@ import android.telephony.TelephonyManager;
 
 public class Device extends Plugin {
 	
-	public static String phonegapVersion = "0.9.3";				// PhoneGap version
+	public static String phonegapVersion = "0.9.4";				// PhoneGap version
 	public static String platform = "Android";					// Device OS
 	public static String uuid;									// Device UUID
     
