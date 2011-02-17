1711fb07d777160fdbde8f6d6bc2deace1a1464f
Fil Maj <filip.maj@nitobi.com>
2011-02-16 16:26:01 -0800

    Small patch to build script: ruby needs double quotes to interpolate variables into it properly.

--- lib/create.rb
+++ lib/create.rb
@@ -29,7 +29,7 @@ class Create < Classic
     @content          = 'index.html'
     
     # stop executation on errors
-    raise 'Expected index.html in the following folder #{ path }.\nThe path is expected to be the directory droidgap create is run from or specified as a command line arg like droidgap create my_path.' unless File.exists? File.join(path, 'index.html')    
+    raise "Expected index.html in the following folder #{ path }.\nThe path is expected to be the directory droidgap create is run from or specified as a command line arg like droidgap create my_path." unless File.exists? File.join(path, 'index.html')    
     raise 'Could not find android in your PATH!' if @android_sdk_path.empty?
   end
 
