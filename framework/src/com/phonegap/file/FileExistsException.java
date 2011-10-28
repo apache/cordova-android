e1d3f1491fed995fe5dca6d7d6d3fbccc4a19402
Joe Bowser <joe.bowser@nitobi.com>
2011-10-28 16:31:48 -0700

    Accidentally added stray char when changing the licence

--- framework/src/com/phonegap/file/FileExistsException.java
+++ framework/src/com/phonegap/file/FileExistsException.java
@@ -16,7 +16,6 @@
        specific language governing permissions and limitations
        under the License.
 */
-p
 
 package com.phonegap.file;
 
--- framework/src/com/phonegap/file/InvalidModificationException.java
+++ framework/src/com/phonegap/file/InvalidModificationException.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-p
+
 
 package com.phonegap.file;
 
