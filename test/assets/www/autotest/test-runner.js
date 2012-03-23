8ecfcb12c7e1e68b8e11da50788029b8e69ee495
Joe Bowser <bowserj@apache.org>
2012-03-23 14:57:58 -0700

    Cleaning out the asssets/www directory.  This should house mobile-spec

--- test/assets/www/autotest/test-runner.js
+++
@@ -1,9 +0,0 @@
-// Prevent QUnit from running when the DOM load event fires
-QUnit.config.autostart = false;
-sessionStorage.clear();
-
-// Timeout is 2 seconds to allow physical devices enough
-// time to query the response. This is important for some
-// Android devices.
-var Tests = function() {};
-Tests.TEST_TIMEOUT = 2000;
