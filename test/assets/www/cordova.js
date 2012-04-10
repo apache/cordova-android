36e6b572b7bf8766b797c5a58f3055be09915301
Bryce Curtis <curtis.bryce@gmail.com>
2012-04-10 09:01:37 -0500

    Update to version 1.6.0.

--- test/assets/www/cordova.js
+++ test/assets/www/cordova.js
@@ -1,2 +1,2 @@
-document.write('<script type="text/javascript" charset="utf-8" src="../cordova-1.6.0rc1.js"></script>');
-document.write('<script type="text/javascript" charset="utf-8" src="cordova-1.6.0rc1.js"></script>');
+document.write('<script type="text/javascript" charset="utf-8" src="../cordova-1.6.0.js"></script>');
+document.write('<script type="text/javascript" charset="utf-8" src="cordova-1.6.0.js"></script>');
