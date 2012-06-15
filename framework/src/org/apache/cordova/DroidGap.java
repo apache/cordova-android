07ed6daeda2df0bedfa13d9b82d3aae15147b573
Joe Bowser <bowserj@apache.org>
2012-06-15 15:01:46 -0700

    Removed merge because I missed the preference set

--- framework/src/org/apache/cordova/DroidGap.java
+++ framework/src/org/apache/cordova/DroidGap.java
@@ -256,17 +256,8 @@ public class DroidGap extends Activity implements CordovaInterface {
         LOG.d(TAG, "DroidGap.onCreate()");
         super.onCreate(savedInstanceState);
 
-        if (!preferences.prefMatches("showTitle", "true")) {
-            getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-        }
-
-        if (preferences.prefMatches("fullscreen","true")) {
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        } else {
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN,
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-        }
 
         // This builds the view.  We could probably get away with NOT having a LinearLayout, but I like having a bucket!
         Display display = getWindowManager().getDefaultDisplay();
