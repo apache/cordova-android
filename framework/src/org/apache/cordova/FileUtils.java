6c594b6f5f22a3718b9f59c95854ef4cdd4ad614
macdonst <simon.macdonald@gmail.com>
2012-06-19 11:29:00 -0400

    Fixing merge error in FileUtils.notifyDelete

--- framework/src/org/apache/cordova/FileUtils.java
+++ framework/src/org/apache/cordova/FileUtils.java
@@ -225,7 +225,7 @@ public class FileUtils extends Plugin {
         String newFilePath = stripFileProtocol(filePath);
         int result = this.cordova.getActivity().getContentResolver().delete(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
             MediaStore.Images.Media.DATA + " = ?",
-            new String[] { filePath });
+            new String[] { newFilePath });
     }
 
     /**
