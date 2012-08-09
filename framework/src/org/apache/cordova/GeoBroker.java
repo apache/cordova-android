b297fe6f597358713fb2dbf70253d54552f25283
Andrew Grieve <agrieve@chromium.org>
2012-08-08 23:39:02 -0400

    Fix a NPE in GeoBroker when there is no previous location.

--- framework/src/org/apache/cordova/GeoBroker.java
+++ framework/src/org/apache/cordova/GeoBroker.java
@@ -75,7 +75,7 @@ public class GeoBroker extends Plugin {
 	                int maximumAge = args.getInt(1);
 	                Location last = this.locationManager.getLastKnownLocation((enableHighAccuracy ? LocationManager.GPS_PROVIDER : LocationManager.NETWORK_PROVIDER));
 	                // Check if we can use lastKnownLocation to get a quick reading and use less battery
-	                if ((System.currentTimeMillis() - last.getTime()) <= maximumAge) {
+	                if (last != null && (System.currentTimeMillis() - last.getTime()) <= maximumAge) {
 	                    result = new PluginResult(PluginResult.Status.OK, this.returnLocationJSON(last));
 	                } else {
 	                    this.getCurrentLocation(callbackId, enableHighAccuracy);
