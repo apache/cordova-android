406bf6d8cebdc3797da8273f493da032070bc29c
James Jong <wjamesjong@gmail.com>
2013-02-06 19:03:09 -0500

    CB-1944: Better error messages for Create script
    - fixed to detect missing packages individually
    - added a specific message for each missing package
    - messages include how to correct and package download link

--- bin/create.bat
+++ bin/create.bat
@@ -16,17 +16,34 @@
 :: under the License.
 
 @ECHO OFF
-IF NOT DEFINED JAVA_HOME GOTO MISSING
+IF NOT DEFINED JAVA_HOME GOTO MISSING_JAVA_HOME
+
 FOR %%X in (java.exe javac.exe ant.bat android.bat) do (
-    SET FOUND=%%~$PATH:X
-    IF NOT DEFINED FOUND GOTO MISSING
+    IF [%%~$PATH:X]==[] (
+      ECHO Cannot locate %%X using the PATH environment variable.
+      ECHO Retry after adding directory containing %%X to the PATH variable.
+      IF "%%X"=="java.exe" GOTO GET_JAVA
+      IF "%%X"=="javac.exe" GOTO GET_JAVA
+      IF "%%X"=="ant.bat" GOTO GET_ANT
+      IF "%%X"=="android.bat" GOTO GET_ANDROID
+      GOTO ERROR
+    )
 )
 cscript "%~dp0\create.js" %*
 GOTO END
-:MISSING
-ECHO Missing one of the following:
-ECHO JDK: http://java.oracle.com
-ECHO Android SDK: http://developer.android.com
-ECHO Apache ant: http://ant.apache.org
+:MISSING_JAVA_HOME
+	ECHO The JAVA_HOME environment variable is not set.
+	ECHO Retry after setting JAVA_HOME to an existing JRE directory.
+	GOTO ERROR
+:GET_JAVA
+        ECHO Visit http://java.oracle.com if you need to install Java (JDK).
+	GOTO ERROR
+:GET_ANT
+        ECHO Visit http://ant.apache.org if you need to install Apache Ant.
+	GOTO ERROR
+:GET_ANDROID
+        ECHO Visit http://developer.android.com if you need to install the Android SDK.
+	GOTO ERROR
+:ERROR
 EXIT /B 1
 :END
