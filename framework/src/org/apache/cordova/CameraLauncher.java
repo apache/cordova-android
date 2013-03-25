4ab4606ad268ae0fde03e9d3f641202293cf0948
Joe Bowser <bowserj@apache.org>
2013-03-25 16:04:22 -0700

    Fixing CB-1700, we had the file names reversed, so exif was never being written right.  Needed to upload a file to debug this thing

--- framework/src/org/apache/cordova/CameraLauncher.java
+++ framework/src/org/apache/cordova/CameraLauncher.java
@@ -444,7 +444,7 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
                                     ExifHelper exif = new ExifHelper();
                                     try {
                                         if (this.encodingType == JPEG) {
-                                            exif.createInFile(resizePath);
+                                            exif.createInFile(FileHelper.getRealPath(uri, this.cordova));
                                             exif.readExifData();
                                             rotate = exif.getOrientation();
                                         }
@@ -458,7 +458,7 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
 
                                     // Restore exif data to file
                                     if (this.encodingType == JPEG) {
-                                        exif.createOutFile(FileHelper.getRealPath(uri, this.cordova));
+                                        exif.createOutFile(resizePath);
                                         exif.writeExifData();
                                     }
 
