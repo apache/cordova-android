420cee4ee6f2aeed43b8f54423f22791bb84141d
Andrew Grieve <agrieve@chromium.org>
2013-05-28 16:43:38 -0400

    [CB-3468] Add missing license headers found by RAT

--- bin/templates/cordova/cordova.bat
+++ bin/templates/cordova/cordova.bat
@@ -1,5 +1,3 @@
-@ECHO OFF
-GOTO BEGIN
 :: Licensed to the Apache Software Foundation (ASF) under one
 :: or more contributor license agreements.  See the NOTICE file
 :: distributed with this work for additional information
@@ -16,7 +14,7 @@ GOTO BEGIN
 :: KIND, either express or implied.  See the License for the
 :: specific language governing permissions and limitations
 :: under the License.
-:BEGIN
+@ECHO OFF
 IF NOT DEFINED JAVA_HOME GOTO MISSING
 FOR %%X in (java.exe ant.bat android.bat) do (
     SET FOUND=%%~$PATH:X
