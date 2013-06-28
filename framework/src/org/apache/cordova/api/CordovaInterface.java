7cde444ffe0085b2100a8ceedf8fa2d40369fde5
Andrew Grieve <agrieve@chromium.org>
2013-06-27 22:29:18 -0400

    [CB-4038] Move non-deprecated classes from the api package into the main package. (cherry picked from commit 79829f620985249876bdb7fefa572a53435da336)

--- framework/src/org/apache/cordova/api/CordovaInterface.java
+++ framework/src/org/apache/cordova/api/CordovaInterface.java
@@ -18,53 +18,9 @@
 */
 package org.apache.cordova.api;
 
-import android.app.Activity;
-import android.content.Intent;
-
-import java.util.concurrent.ExecutorService;
-
 /**
  * The Activity interface that is implemented by CordovaActivity.
  * It is used to isolate plugin development, and remove dependency on entire Cordova library.
  */
-public interface CordovaInterface {
-
-    /**
-     * Launch an activity for which you would like a result when it finished. When this activity exits,
-     * your onActivityResult() method will be called.
-     *
-     * @param command     The command object
-     * @param intent      The intent to start
-     * @param requestCode   The request code that is passed to callback to identify the activity
-     */
-    abstract public void startActivityForResult(CordovaPlugin command, Intent intent, int requestCode);
-
-    /**
-     * Set the plugin to be called when a sub-activity exits.
-     *
-     * @param plugin      The plugin on which onActivityResult is to be called
-     */
-    abstract public void setActivityResultCallback(CordovaPlugin plugin);
-
-    /**
-     * Get the Android activity.
-     *
-     * @return
-     */
-    public abstract Activity getActivity();
-    
-
-    /**
-     * Called when a message is sent to plugin.
-     *
-     * @param id            The message id
-     * @param data          The message data
-     * @return              Object or null
-     */
-    public Object onMessage(String id, Object data);
-    
-    /**
-     * Returns a shared thread pool that can be used for background tasks.
-     */
-    public ExecutorService getThreadPool();
+public interface CordovaInterface extends org.apache.cordova.CordovaInterface {
 }
