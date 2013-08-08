8c4c22c72a36acb9cc8c3bbba0e324f7e00d6a35
Max Woghiren <max.woghiren@gmail.com>
2013-08-08 00:53:01 -0400

    [CB-4013] Added a missing import.

--- framework/src/org/apache/cordova/CordovaWebView.java
+++ framework/src/org/apache/cordova/CordovaWebView.java
@@ -23,6 +23,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Stack;
 import java.util.regex.Pattern;
 
