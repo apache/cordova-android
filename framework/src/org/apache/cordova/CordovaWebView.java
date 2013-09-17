2c6285d4b32c990943a5901ceb5e031757d3d38f
Andrew Grieve <agrieve@chromium.org>
2013-09-17 11:03:51 -0400

    Set VERSION to 3.2.0-dev (via coho)

--- framework/src/org/apache/cordova/CordovaWebView.java
+++ framework/src/org/apache/cordova/CordovaWebView.java
@@ -61,7 +61,7 @@ public class CordovaWebView extends WebView {
     
     public static final String TAG = "CordovaWebView";
     
-    public static final String CORDOVA_VERSION = "3.1.0-dev";
+    public static final String CORDOVA_VERSION = "3.2.0-dev";
 
     private ArrayList<Integer> keyDownCodes = new ArrayList<Integer>();
     private ArrayList<Integer> keyUpCodes = new ArrayList<Integer>();
