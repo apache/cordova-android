41ed18684d7cc78e4a6874ac9f92eda50d4c8a06
Carlos Santana <csantana23@gmail.com>
2013-10-29 16:29:19 -0400

    CB-5209 Win: Cannot build Android app if project path contains spaces

--- bin/templates/cordova/build.bat
+++ bin/templates/cordova/build.bat
@@ -5,9 +5,9 @@
 :: to you under the Apache License, Version 2.0 (the
 :: "License"); you may not use this file except in compliance
 :: with the License.  You may obtain a copy of the License at
-:: 
+::
 :: http://www.apache.org/licenses/LICENSE-2.0
-:: 
+::
 :: Unless required by applicable law or agreed to in writing,
 :: software distributed under the License is distributed on an
 :: "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -18,7 +18,7 @@
 @ECHO OFF
 SET script_path="%~dp0build"
 IF EXIST %script_path% (
-        node "%script_path%" %*
+        node %script_path% %*
 ) ELSE (
     ECHO.
     ECHO ERROR: Could not find 'build' script in 'cordova' folder, aborting...>&2
