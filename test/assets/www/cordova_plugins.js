e5b68f4a3c44064e5b20d6d6edc4479aee336ccb
Joe Bowser <bowserj@apache.org>
2013-11-12 14:27:09 -0800

    Fixing plugins configuration, Device shouldn't be checked in here

--- test/assets/www/cordova_plugins.js
+++ test/assets/www/cordova_plugins.js
@@ -1,11 +1,3 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/org.apache.cordova.device/www/device.js",
-        "id": "org.apache.cordova.device.device",
-        "clobbers": [
-            "device"
-        ]
-    }
-]
+module.exports = []
 });
