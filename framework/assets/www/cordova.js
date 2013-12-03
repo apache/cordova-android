7c566c36f4018f02b13ef9f5f7719e2bbf8b8d4b
Joe Bowser <bowserj@apache.org>
2013-12-03 15:44:23 -0800

    Update JS snapshot to version 3.4.0-dev (via coho)

--- framework/assets/www/cordova.js
+++ framework/assets/www/cordova.js
@@ -1,5 +1,5 @@
 // Platform: android
-// 3.3.0-dev-aac4947
+// 3.3.0-dev-c9de1bc
 /*
  Licensed to the Apache Software Foundation (ASF) under one
  or more contributor license agreements.  See the NOTICE file
@@ -19,7 +19,7 @@
  under the License.
 */
 ;(function() {
-var CORDOVA_JS_BUILD_LABEL = '3.3.0-dev-aac4947';
+var CORDOVA_JS_BUILD_LABEL = '3.3.0-dev-c9de1bc';
 // file: lib/scripts/require.js
 
 /*jshint -W079 */
