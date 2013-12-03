214934cc032f765e14bfd54a690cf40f511b7906
Joe Bowser <bowserj@apache.org>
2013-12-03 15:44:20 -0800

    Set VERSION to 3.3.0-rc1 (via coho)

--- framework/src/org/apache/cordova/CordovaWebView.java
+++ framework/src/org/apache/cordova/CordovaWebView.java
@@ -68,7 +68,7 @@ import android.widget.FrameLayout;
 public class CordovaWebView extends WebView {
 
     public static final String TAG = "CordovaWebView";
-    public static final String CORDOVA_VERSION = "3.3.0-dev";
+    public static final String CORDOVA_VERSION = "3.3.0-rc1";
 
     private ArrayList<Integer> keyDownCodes = new ArrayList<Integer>();
     private ArrayList<Integer> keyUpCodes = new ArrayList<Integer>();
