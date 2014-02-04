af440460e10da89caa167ecb5c266f3857493f90
Andrew Grieve <agrieve@chromium.org>
2014-02-04 10:00:58 -0500

    Fix broken build from prev. commit (copy & paste error?)

--- framework/src/org/apache/cordova/CordovaWebViewClient.java
+++ framework/src/org/apache/cordova/CordovaWebViewClient.java
@@ -62,7 +62,7 @@ public class CordovaWebViewClient extends WebViewClient {
     CordovaInterface cordova;
     CordovaWebView appView;
     private boolean doClearHistory = false;
-    private boolean isCurrentlyLoading;
+    boolean isCurrentlyLoading;
 
     /** The authorization tokens. */
     private Hashtable<String, AuthenticationToken> authenticationTokens = new Hashtable<String, AuthenticationToken>();
