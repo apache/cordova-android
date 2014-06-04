eb623a84d517327e62ed21e8a938af6ed279ac85
Joe Bowser <bowserj@apache.org>
2014-06-04 11:13:37 -0700

    Trivial spelling fix in comments when reading CordovaResourceApi

--- framework/src/org/apache/cordova/CordovaResourceApi.java
+++ framework/src/org/apache/cordova/CordovaResourceApi.java
@@ -106,6 +106,7 @@ public class CordovaResourceApi {
         return threadCheckingEnabled;
     }
     
+    
     public static int getUriType(Uri uri) {
         assertNonRelative(uri);
         String scheme = uri.getScheme();
@@ -199,6 +200,8 @@ public class CordovaResourceApi {
         return null;
     }
     
+    
+    //This already exists
     private String getMimeTypeFromPath(String path) {
         String extension = path;
         int lastDot = extension.lastIndexOf('.');
@@ -217,7 +220,7 @@ public class CordovaResourceApi {
     }
     
     /**
-     * Opens a stream to the givne URI, also providing the MIME type & length.
+     * Opens a stream to the given URI, also providing the MIME type & length.
      * @return Never returns null.
      * @throws Throws an InvalidArgumentException for relative URIs. Relative URIs should be
      *     resolved before being passed into this function.
@@ -229,7 +232,7 @@ public class CordovaResourceApi {
     }
 
     /**
-     * Opens a stream to the givne URI, also providing the MIME type & length.
+     * Opens a stream to the given URI, also providing the MIME type & length.
      * @return Never returns null.
      * @throws Throws an InvalidArgumentException for relative URIs. Relative URIs should be
      *     resolved before being passed into this function.
