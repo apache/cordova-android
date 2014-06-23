297f862ccc20de9506a5d5296f98ce5724dc43c0
Andrew Grieve <agrieve@chromium.org>
2014-06-23 14:18:54 -0400

    Delete explicit activity.finish() in back button handling. No change in behaviour.
    The default handling calls through to Activity.onBackPressed(), which
    by default results in activity.finish(), but can be customized by the
    app.

--- framework/src/org/apache/cordova/CordovaWebView.java
+++ framework/src/org/apache/cordova/CordovaWebView.java
@@ -775,12 +775,6 @@ public class CordovaWebView extends WebView {
                         return true;
                     }
                     // If not, then invoke default behavior
-                    else {
-                        //this.activityState = ACTIVITY_EXITING;
-                    	//return false;
-                    	// If they hit back button when app is initializing, app should exit instead of hang until initialization (CB2-458)
-                    	this.cordova.getActivity().finish();
-                    }
                 }
             }
         }
