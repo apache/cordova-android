6f21a96238a298a94cb66bfa4f2a969f768cea69
Joe Bowser <bowserj@apache.org>
2014-06-24 12:57:46 -0700

    Update the errorurl to no longer use intents

--- framework/src/org/apache/cordova/Config.java
+++ framework/src/org/apache/cordova/Config.java
@@ -20,21 +20,16 @@
 package org.apache.cordova;
 
 import java.io.IOException;
-
 import java.util.Locale;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.cordova.LOG;
-
 import org.xmlpull.v1.XmlPullParserException;
 
 import android.app.Activity;
-
 import android.content.res.XmlResourceParser;
 import android.graphics.Color;
-
 import android.util.Log;
 
 public class Config {
@@ -43,6 +38,8 @@ public class Config {
 
     private Whitelist whitelist = new Whitelist();
     private String startUrl;
+
+    private static String errorUrl;
 
     private static Config self = null;
 
@@ -156,6 +153,10 @@ public class Config {
                         boolean value = xml.getAttributeValue(null, "value").equals("true");
                         action.getIntent().putExtra(name, value);
                     }
+                    else if(name.equalsIgnoreCase("errorurl"))
+                    {
+                        errorUrl = xml.getAttributeValue(null, "value");
+                    }
                     else
                     {
                         String value = xml.getAttributeValue(null, "value");
@@ -229,5 +230,9 @@ public class Config {
             return "file:///android_asset/www/index.html";
         }
         return self.startUrl;
+    }
+
+    public static String getErrorUrl() {
+        return errorUrl;
     }
 }
