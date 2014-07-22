f99a5895694fd0712070118de7bab0bdf7309b24
Ian Clelland <iclelland@chromium.org>
2014-07-22 14:15:10 -0400

    Update JS snapshot to version 3.5.1 (via coho)

--- framework/assets/www/cordova.js
+++ framework/assets/www/cordova.js
@@ -1,5 +1,5 @@
 // Platform: android
-// 3.5.0
+// 3.5.1
 /*
  Licensed to the Apache Software Foundation (ASF) under one
  or more contributor license agreements.  See the NOTICE file
@@ -19,7 +19,7 @@
  under the License.
 */
 ;(function() {
-var CORDOVA_JS_BUILD_LABEL = '3.5.0';
+var CORDOVA_JS_BUILD_LABEL = '3.5.1';
 // file: src/scripts/require.js
 
 /*jshint -W079 */
