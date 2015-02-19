8106981bb6383f55f776d56d07867eb357a9c483
Andrew Grieve <agrieve@chromium.org>
2015-02-19 10:43:25 -0500

    Extract alert, confirm, prompt Dialog logic into a helper for use by other engines

--- framework/src/org/apache/cordova/AndroidChromeClient.java
+++ framework/src/org/apache/cordova/AndroidChromeClient.java
@@ -18,31 +18,24 @@
 */
 package org.apache.cordova;
 
-import org.apache.cordova.CordovaInterface;
-import org.apache.cordova.LOG;
-
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.util.Log;
 import android.view.Gravity;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.webkit.ConsoleMessage;
+import android.webkit.GeolocationPermissions.Callback;
 import android.webkit.JsPromptResult;
 import android.webkit.JsResult;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebStorage;
 import android.webkit.WebView;
-import android.webkit.GeolocationPermissions.Callback;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
@@ -52,11 +45,6 @@ import android.widget.RelativeLayout;
  * The kind of callbacks that happen here are on the chrome outside the document,
  * such as onCreateWindow(), onConsoleMessage(), onProgressChanged(), etc. Related
  * to but different than CordovaWebViewClient.
- *
- * @see <a href="http://developer.android.com/reference/android/webkit/WebChromeClient.html">WebChromeClient</a>
- * @see <a href="http://developer.android.com/guide/webapps/webview.html">WebView guide</a>
- * @see CordovaWebViewClient
- * @see CordovaWebView
  */
 public class AndroidChromeClient extends WebChromeClient {
 
@@ -69,104 +57,46 @@ public class AndroidChromeClient extends WebChromeClient {
     // the video progress view
     private View mVideoProgressView;
     
-    //Keep track of last AlertDialog showed
-    private AlertDialog lastHandledDialog;
+    private CordovaDialogsHelper dialogsHelper;
 
-    public AndroidChromeClient(CordovaInterface ctx, AndroidWebView app) {
+    public AndroidChromeClient(CordovaInterface ctx, AndroidWebView webView) {
         this.cordova = ctx;
-        this.appView = app;
+        this.appView = webView;
+        dialogsHelper = new CordovaDialogsHelper(webView.getContext());
     }
 
     /**
      * Tell the client to display a javascript alert dialog.
-     *
-     * @param view
-     * @param url
-     * @param message
-     * @param result
-     * @see Other implementation in the Dialogs plugin.
      */
     @Override
     public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
-        AlertDialog.Builder dlg = new AlertDialog.Builder(this.cordova.getActivity());
-        dlg.setMessage(message);
-        dlg.setTitle("Alert");
-        //Don't let alerts break the back button
-        dlg.setCancelable(true);
-        dlg.setPositiveButton(android.R.string.ok,
-                new AlertDialog.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        result.confirm();
-                    }
-                });
-        dlg.setOnCancelListener(
-                new DialogInterface.OnCancelListener() {
-                    public void onCancel(DialogInterface dialog) {
-                        result.cancel();
-                    }
-                });
-        dlg.setOnKeyListener(new DialogInterface.OnKeyListener() {
-            //DO NOTHING
-            public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
-                if (keyCode == KeyEvent.KEYCODE_BACK)
-                {
+        dialogsHelper.showAlert(message, new CordovaDialogsHelper.Result() {
+            @Override public void gotResult(boolean success, String value) {
+                if (success) {
                     result.confirm();
-                    return false;
+                } else {
+                    result.cancel();
                 }
-                else
-                    return true;
             }
         });
-        lastHandledDialog = dlg.show();
         return true;
     }
 
     /**
      * Tell the client to display a confirm dialog to the user.
-     *
-     * @param view
-     * @param url
-     * @param message
-     * @param result
-     * @see Other implementation in the Dialogs plugin.
      */
     @Override
     public boolean onJsConfirm(WebView view, String url, String message, final JsResult result) {
-        AlertDialog.Builder dlg = new AlertDialog.Builder(this.cordova.getActivity());
-        dlg.setMessage(message);
-        dlg.setTitle("Confirm");
-        dlg.setCancelable(true);
-        dlg.setPositiveButton(android.R.string.ok,
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        result.confirm();
-                    }
-                });
-        dlg.setNegativeButton(android.R.string.cancel,
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        result.cancel();
-                    }
-                });
-        dlg.setOnCancelListener(
-                new DialogInterface.OnCancelListener() {
-                    public void onCancel(DialogInterface dialog) {
-                        result.cancel();
-                    }
-                });
-        dlg.setOnKeyListener(new DialogInterface.OnKeyListener() {
-            //DO NOTHING
-            public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
-                if (keyCode == KeyEvent.KEYCODE_BACK)
-                {
+        dialogsHelper.showConfirm(message, new CordovaDialogsHelper.Result() {
+            @Override
+            public void gotResult(boolean success, String value) {
+                if (success) {
+                    result.confirm();
+                } else {
                     result.cancel();
-                    return false;
                 }
-                else
-                    return true;
             }
         });
-        lastHandledDialog = dlg.show();
         return true;
     }
 
@@ -177,40 +107,24 @@ public class AndroidChromeClient extends WebChromeClient {
      *
      * Since we are hacking prompts for our own purposes, we should not be using them for
      * this purpose, perhaps we should hack console.log to do this instead!
-     *
-     * @see Other implementation in the Dialogs plugin.
      */
     @Override
-    public boolean onJsPrompt(WebView view, String origin, String message, String defaultValue, JsPromptResult result) {
+    public boolean onJsPrompt(WebView view, String origin, String message, String defaultValue, final JsPromptResult result) {
         // Unlike the @JavascriptInterface bridge, this method is always called on the UI thread.
         String handledRet = appView.bridge.promptOnJsPrompt(origin, message, defaultValue);
         if (handledRet != null) {
             result.confirm(handledRet);
         } else {
-            // Returning false would also show a dialog, but the default one shows the origin (ugly).
-            final JsPromptResult res = result;
-            AlertDialog.Builder dlg = new AlertDialog.Builder(this.cordova.getActivity());
-            dlg.setMessage(message);
-            final EditText input = new EditText(this.cordova.getActivity());
-            if (defaultValue != null) {
-                input.setText(defaultValue);
-            }
-            dlg.setView(input);
-            dlg.setCancelable(false);
-            dlg.setPositiveButton(android.R.string.ok,
-                    new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            String usertext = input.getText().toString();
-                            res.confirm(usertext);
-                        }
-                    });
-            dlg.setNegativeButton(android.R.string.cancel,
-                    new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            res.cancel();
-                        }
-                    });
-            lastHandledDialog = dlg.show();
+            dialogsHelper.showPrompt(message, defaultValue, new CordovaDialogsHelper.Result() {
+                @Override
+                public void gotResult(boolean success, String value) {
+                    if (success) {
+                        result.confirm(value);
+                    } else {
+                        result.cancel();
+                    }
+                }
+            });
         }
         return true;
     }
@@ -348,9 +262,7 @@ public class AndroidChromeClient extends WebChromeClient {
     }
 
     public void destroyLastDialog(){
-        if(lastHandledDialog != null){
-                lastHandledDialog.cancel();
-        }
+        dialogsHelper.destroyLastDialog();
     }
 
 }
