1efc05d3e81523750c74753efcbaf26aaac6b6f0
Kenneth Chan <kenneth@loqheart.com>
2015-04-23 14:40:37 -0700

    use immersive mode when available and "fullscreen" is set to 'true'

--- framework/src/org/apache/cordova/CordovaActivity.java
+++ framework/src/org/apache/cordova/CordovaActivity.java
@@ -31,6 +31,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -115,8 +116,24 @@ public class CordovaActivity extends Activity {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
         } else if (preferences.getBoolean("Fullscreen", false)) {
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+            {
+                setSystemUiVisibilityMode(getWindow());
+                getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                            setSystemUiVisibilityMode(getWindow());
+                        }
+                    }
+                });
+            }
+            else
+            {
+                getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            }
         } else {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
@@ -129,6 +146,18 @@ public class CordovaActivity extends Activity {
         {
             cordovaInterface.restoreInstanceState(savedInstanceState);
         }
+    }
+
+    private void setSystemUiVisibilityMode(Window window) {
+        final int uiOptions =
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        window.getDecorView().setSystemUiVisibility(uiOptions);
     }
     
     protected void init() {
