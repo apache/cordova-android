2a2c8b4fbacc4707104f3f2740b1f80e1fbf8d42
Simon Pireyn <spireyn@gmail.com>
2015-07-08 17:34:21 +0200

    CB-9334 loop on CordovaPlugins before returning remapped Uri

--- framework/src/org/apache/cordova/PluginManager.java
+++ framework/src/org/apache/cordova/PluginManager.java
@@ -18,21 +18,14 @@
  */
 package org.apache.cordova;
 
-import java.util.HashMap;
-import java.util.List;
-
-import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.PluginEntry;
-import org.apache.cordova.PluginResult;
-import org.json.JSONException;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Debug;
 import android.util.Log;
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * PluginManager is exposed to JavaScript in the Cordova WebView.
@@ -381,15 +374,17 @@ public class PluginManager {
     }
 
     Uri remapUri(Uri uri) {
+        Uri remap = null;
         for (CordovaPlugin plugin : this.pluginMap.values()) {
             if (plugin != null) {
                 Uri ret = plugin.remapUri(uri);
                 if (ret != null) {
-                    return ret;
+                    remap = ret;
+                    uri = ret;
                 }
             }
         }
-        return null;
+        return remap;
     }
 
     /**
