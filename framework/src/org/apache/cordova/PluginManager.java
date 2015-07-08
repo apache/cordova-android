3e8948650c6a42487c980b7ee396c966a9533878
Simon Pireyn <spireyn@gmail.com>
2015-07-08 17:54:52 +0200

    Check is not bitwise

--- framework/src/org/apache/cordova/PluginManager.java
+++ framework/src/org/apache/cordova/PluginManager.java
@@ -396,7 +396,7 @@ public class PluginManager {
             if ((className != null) && !("".equals(className))) {
                 c = Class.forName(className);
             }
-            if (c != null & CordovaPlugin.class.isAssignableFrom(c)) {
+            if (c != null && CordovaPlugin.class.isAssignableFrom(c)) {
                 ret = (CordovaPlugin) c.newInstance();
             }
         } catch (Exception e) {
