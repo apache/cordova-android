78fa7374d97ad9ed85c5c857a77a8f3830d600f9
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2015-10-28 12:39:54 +0300

    CB-9880 Fixes platform update failure when upgrading from android@<4.1.0

--- bin/lib/create.js
+++ bin/lib/create.js
@@ -309,7 +309,7 @@ exports.update = function(projectPath, options, events) {
 
         if (Number(manifest.getMinSdkVersion()) < MIN_SDK_VERSION) {
             events.emit('verbose', 'Updating minSdkVersion to ' + MIN_SDK_VERSION + ' in AndroidManifest.xml');
-            manifest.setMinSDKVersion(MIN_SDK_VERSION);
+            manifest.setMinSdkVersion(MIN_SDK_VERSION);
         }
 
         manifest.setDebuggable(false).write();
