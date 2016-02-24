2bf47422ce7b307bc780c09bf130554a6d988322
Steve Gill <stevengill97@gmail.com>
2016-02-24 11:50:06 -0800

    Update JS snapshot to version 5.1.1 (via coho)

--- bin/templates/project/assets/www/cordova.js
+++ bin/templates/project/assets/www/cordova.js
@@ -19,7 +19,7 @@
  under the License.
 */
 ;(function() {
-var PLATFORM_VERSION_BUILD_LABEL = '5.2.0-dev';
+var PLATFORM_VERSION_BUILD_LABEL = '5.1.1';
 // file: src/scripts/require.js
 
 /*jshint -W079 */
