eccde6c0c02bff61fcd8d57c39f2cf0a62dbc079
Joe Bowser <bowserj@apache.org>
2016-03-22 12:45:23 -0700

    CB-10896: We never enabled cookies on the WebView proper

--- framework/src/org/apache/cordova/engine/SystemCookieManager.java
+++ framework/src/org/apache/cordova/engine/SystemCookieManager.java
@@ -37,6 +37,9 @@ class SystemCookieManager implements ICordovaCookieManager {
         webView = webview;
         cookieManager = CookieManager.getInstance();
 
+        //REALLY? Nobody has seen this UNTIL NOW?
+        cookieManager.setAcceptFileSchemeCookies(true);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             cookieManager.setAcceptThirdPartyCookies(webView, true);
         }
