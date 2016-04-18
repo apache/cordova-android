2a8a6b39708df9ffb2d4456617851f57a22add0d
dheeran1 <dheeran@hotmail.com>
2016-04-18 20:15:41 +0530

    Update CoreAndroid.java
    No modifier (package-private) class to public class
    
    This closes #293

--- framework/src/org/apache/cordova/CoreAndroid.java
+++ framework/src/org/apache/cordova/CoreAndroid.java
@@ -35,7 +35,7 @@ import java.util.HashMap;
 /**
  * This class exposes methods in Cordova that can be called from JavaScript.
  */
-class CoreAndroid extends CordovaPlugin {
+public class CoreAndroid extends CordovaPlugin {
 
     public static final String PLUGIN_NAME = "CoreAndroid";
     protected static final String TAG = "CordovaApp";
