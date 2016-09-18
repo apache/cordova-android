a4f6c8bf7bf5ba49855f31831662f7964b17f962
Simon MacDonald <simon.macdonald@gmail.com>
2016-09-18 16:00:53 -0400

    Enable background start of Cordova Android apps
    This closes #322

--- bin/templates/project/Activity.java
+++ bin/templates/project/Activity.java
@@ -28,6 +28,13 @@ public class __ACTIVITY__ extends CordovaActivity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        // enable Cordova apps to be started in the background
+        Bundle extras = getIntent().getExtras();
+        if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
+        		moveTaskToBack(true);
+        }
+
         // Set by <content src="index.html" /> in config.xml
         loadUrl(launchUrl);
     }
