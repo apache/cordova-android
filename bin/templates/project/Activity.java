77b9fd62a723a1f9b86955578e454447311cbe05
Simon MacDonald <simon.macdonald@gmail.com>
2016-10-12 22:21:14 -0400

    Fix tab/space issue

--- bin/templates/project/Activity.java
+++ bin/templates/project/Activity.java
@@ -32,7 +32,7 @@ public class __ACTIVITY__ extends CordovaActivity
         // enable Cordova apps to be started in the background
         Bundle extras = getIntent().getExtras();
         if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
-        		moveTaskToBack(true);
+            moveTaskToBack(true);
         }
 
         // Set by <content src="index.html" /> in config.xml
