0fd7e7f040a487d363c84062eb77768320a2c006
Jesse MacFadyen <purplecabbage@gmail.com>
2016-11-02 17:55:32 -0700

    Fixed AndroidStudio tests to actually run, removed app/src/main/assets/ as a requirement and added app/src/main/res instead, added placeholder for build/ folder, Removed dupe gitignore

---
+++ spec/unit/AndroidStudio.spec.js
@@ -0,0 +1,17 @@
+
+var path = require('path');
+var AndroidStudio = require('../../bin/templates/cordova/lib/AndroidStudio');
+
+
+describe('AndroidStudio module', function () {
+    it('should return true for Android Studio project', function() {
+      var root = path.join(__dirname,'../fixtures/android_studio_project/');
+      var isAndStud = AndroidStudio.isAndroidStudioProject(root);
+      expect(isAndStud).toBe(true);
+    });
+    it('should return false non Android Studio project', function() {
+      var root = path.join(__dirname,'../fixtures/android_project/');
+      var isAndStud = AndroidStudio.isAndroidStudioProject(root);
+      expect(isAndStud).toBe(false);
+    });
+});
