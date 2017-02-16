dc945747a07d112bd2321fff6e4a17cef0da9319
Joe Bowser <bowserj@apache.org>
2017-02-15 16:55:38 -0800

    Adding basic intent tests

--- UnitTests/app/src/androidTest/java/org/apache/cordova/unittests/EmbeddedWebViewTest.java
+++ UnitTests/app/src/androidTest/java/org/apache/cordova/unittests/EmbeddedWebViewTest.java
@@ -1,19 +1,41 @@
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+*/
+
 package org.apache.cordova.unittests;
 
-import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.apache.cordova.CordovaWebView;
-import org.junit.Before;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.web.sugar.Web.onWebView;
-import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
+
+/*
+ * This test is to cover the use case of Cordova Android used as a component in a larger Android
+ * application.  This test is strictly used to cover this use case.  In this example, the WebView
+ * should load, not be null, and the Plugin Manager should also be initialized.
+ *
+
+ */
+
 
 @RunWith(AndroidJUnit4.class)
 public class EmbeddedWebViewTest {
