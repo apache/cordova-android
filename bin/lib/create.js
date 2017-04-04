8ead919faea814e86e8f665e9122a35bcd82d869
Joe Bowser <bowserj@apache.org>
2017-04-04 15:42:58 -0700

    Managed to get the project to mostly compile, still need to re-work the build command to add the app project

--- bin/lib/create.js
+++ bin/lib/create.js
@@ -135,7 +135,7 @@ function copyBuildRules(projectPath) {
     var srcDir = path.join(ROOT, 'bin', 'templates', 'project');
 
     shell.cp('-f', path.join(srcDir, 'build.gradle'), projectPath);
-    shell.cp('-f', path.join(srcDir, 'app', 'build.gradle'), projectPath);
+    shell.cp('-f', path.join(srcDir, 'app', 'build.gradle'), path.join(projectPath, 'app'));
     shell.cp('-f', path.join(srcDir, 'wrapper.gradle'), projectPath);
 }
 
