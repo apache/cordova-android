cf159c1ae392783fad2219ca3708bf4fe9ccc890
Steve Gill <stevengill97@gmail.com>
2017-04-24 22:05:42 -0700

    Update JS snapshot to version 6.2.2 (via coho)

--- bin/templates/project/assets/www/cordova.js
+++ bin/templates/project/assets/www/cordova.js
@@ -19,7 +19,7 @@
  under the License.
 */
 ;(function() {
-var PLATFORM_VERSION_BUILD_LABEL = '6.2.1';
+var PLATFORM_VERSION_BUILD_LABEL = '6.2.2';
 // file: src/scripts/require.js
 
 /*jshint -W079 */
