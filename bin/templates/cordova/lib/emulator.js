750531c4bc501b545dcd0b05d9284c9328a889e2
Rizal M. S <rizal.solichudin@gmail.com>
2018-06-25 13:44:37 +0700

    CB-14165 Emulator: handle "device still connecting" error (#457)
    Keep waiting for emulator when connection fails with "device still connecting" error

--- bin/templates/cordova/lib/emulator.js
+++ bin/templates/cordova/lib/emulator.js
@@ -344,7 +344,8 @@ module.exports.wait_for_emulator = function (port) {
         }, function (error) {
             if ((error && error.message &&
             (error.message.indexOf('not found') > -1)) ||
-            (error.message.indexOf('device offline') > -1)) {
+            (error.message.indexOf('device offline') > -1) ||
+            (error.message.indexOf('device still connecting') > -1)) {
                 // emulator not yet started, continue waiting
                 return self.wait_for_emulator(port);
             } else {
