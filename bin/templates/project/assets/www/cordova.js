d62917b66ca8c2e8090f7ebaa7bf41ee55515105
Christopher J. Brody <chris.brody@gmail.com>
2018-07-04 05:37:26 -0400

    CB-9366 log error.stack in cordova.js
    (update JS snapshot from cordova-js@4.2.4 via coho)

--- bin/templates/project/assets/www/cordova.js
+++ bin/templates/project/assets/www/cordova.js
@@ -1,5 +1,5 @@
 // Platform: android
-// f5a790b434f6ac050da55c57e5fd5770f5a16512
+// e040d9e9343d5f27dd4f67616660b33b92a82ff7
 /*
  Licensed to the Apache Software Foundation (ASF) under one
  or more contributor license agreements.  See the NOTICE file
@@ -307,6 +307,7 @@ var cordova = {
         } catch (err) {
             var msg = 'Error in ' + (isSuccess ? 'Success' : 'Error') + ' callbackId: ' + callbackId + ' : ' + err;
             console && console.log && console.log(msg);
+            console && console.log && err.stack && console.log(err.stack);
             cordova.fireWindowEvent('cordovacallbackerror', { 'message': msg });
             throw err;
         }
