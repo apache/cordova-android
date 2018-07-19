ddffa06ee561a1d3296ad5fed5eeceb0c3b1aef9
Bikram Pandit <bikcrum@gmail.com>
2018-07-19 14:44:14 +0545

    Update CordovaResourceApi.java
    Add FileUtils class to support URI to getting real file path for different APIs.

--- framework/src/org/apache/cordova/CordovaResourceApi.java
+++ framework/src/org/apache/cordova/CordovaResourceApi.java
@@ -18,14 +18,21 @@
  */
 package org.apache.cordova;
 
+import android.annotation.SuppressLint;
 import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.os.Looper;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.util.Base64;
+import android.util.Log;
 import android.webkit.MimeTypeMap;
 
 import java.io.ByteArrayInputStream;
@@ -84,9 +91,11 @@ public class CordovaResourceApi {
     private final ContentResolver contentResolver;
     private final PluginManager pluginManager;
     private boolean threadCheckingEnabled = true;
+    private Context context;
 
 
     public CordovaResourceApi(Context context, PluginManager pluginManager) {
+        this.context = context;
         this.contentResolver = context.getContentResolver();
         this.assetManager = context.getAssets();
         this.pluginManager = pluginManager;
@@ -151,20 +160,9 @@ public class CordovaResourceApi {
             case URI_TYPE_FILE:
                 return new File(uri.getPath());
             case URI_TYPE_CONTENT: {
-                Cursor cursor = contentResolver.query(uri, LOCAL_FILE_PROJECTION, null, null, null);
-                if (cursor != null) {
-                    try {
-                        int columnIndex = cursor.getColumnIndex(LOCAL_FILE_PROJECTION[0]);
-                        if (columnIndex != -1 && cursor.getCount() > 0) {
-                            cursor.moveToFirst();
-                            String realPath = cursor.getString(columnIndex);
-                            if (realPath != null) {
-                                return new File(realPath);
-                            }
-                        }
-                    } finally {
-                        cursor.close();
-                    }
+                String filePath = FileUtils.getPath(context, uri);
+                if (filePath != null) {
+                    return new File(filePath);
                 }
             }
         }
@@ -263,7 +261,6 @@ public class CordovaResourceApi {
                 } catch (FileNotFoundException e) {
                     // Will occur if the file is compressed.
                     inputStream = assetManager.open(assetPath);
-                    length = inputStream.available();
                 }
                 String mimeType = getMimeTypeFromPath(assetPath);
                 return new OpenForReadResult(uri, inputStream, mimeType, length, assetFd);
@@ -467,6 +464,138 @@ public class CordovaResourceApi {
             this.mimeType = mimeType;
             this.length = length;
             this.assetFd = assetFd;
+        }
+    }
+
+    private static class FileUtils {
+        /**
+         * Get a file path from a Uri. This will get the the path for Storage Access
+         * Framework Documents, as well as the _data field for the MediaStore and
+         * other file-based ContentProviders.
+         *
+         * @param context The context.
+         * @param uri     The Uri to query.
+         * @author paulburke
+         */
+        @SuppressLint("NewApi")
+        public static String getPath(final Context context, final Uri uri) {
+
+            final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+            // DocumentProvider
+            if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+                // ExternalStorageProvider
+                if (isExternalStorageDocument(uri)) {
+                    final String docId = DocumentsContract.getDocumentId(uri);
+                    final String[] split = docId.split(":");
+                    final String type = split[0];
+
+                    if ("primary".equalsIgnoreCase(type)) {
+                        return Environment.getExternalStorageDirectory() + "/" + split[1];
+                    }
+
+                    // TODO handle non-primary volumes
+                }
+                // DownloadsProvider
+                else if (isDownloadsDocument(uri)) {
+
+                    final String id = DocumentsContract.getDocumentId(uri);
+                    final Uri contentUri = ContentUris.withAppendedId(
+                            Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                    return getDataColumn(context, contentUri, null, null);
+                }
+                // MediaProvider
+                else if (isMediaDocument(uri)) {
+                    final String docId = DocumentsContract.getDocumentId(uri);
+                    final String[] split = docId.split(":");
+                    final String type = split[0];
+
+                    Uri contentUri = null;
+                    if ("image".equals(type)) {
+                        contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                    } else if ("video".equals(type)) {
+                        contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                    } else if ("audio".equals(type)) {
+                        contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                    }
+
+                    final String selection = "_id=?";
+                    final String[] selectionArgs = new String[]{
+                            split[1]
+                    };
+
+                    return getDataColumn(context, contentUri, selection, selectionArgs);
+                }
+            }
+            // MediaStore (and general)
+            else if ("content".equalsIgnoreCase(uri.getScheme())) {
+                return getDataColumn(context, uri, null, null);
+            }
+            // File
+            else if ("file".equalsIgnoreCase(uri.getScheme())) {
+                return uri.getPath();
+            }
+
+            return null;
+        }
+
+        /**
+         * Get the value of the data column for this Uri. This is useful for
+         * MediaStore Uris, and other file-based ContentProviders.
+         *
+         * @param context       The context.
+         * @param uri           The Uri to query.
+         * @param selection     (Optional) Filter used in the query.
+         * @param selectionArgs (Optional) Selection arguments used in the query.
+         * @return The value of the _data column, which is typically a file path.
+         */
+        public static String getDataColumn(Context context, Uri uri, String selection,
+                                           String[] selectionArgs) {
+
+            Cursor cursor = null;
+            final String column = "_data";
+            final String[] projection = {
+                    column
+            };
+
+            try {
+                cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                        null);
+                if (cursor != null && cursor.moveToFirst()) {
+                    final int column_index = cursor.getColumnIndexOrThrow(column);
+                    return cursor.getString(column_index);
+                }
+            } finally {
+                if (cursor != null)
+                    cursor.close();
+            }
+            return null;
+        }
+
+
+        /**
+         * @param uri The Uri to check.
+         * @return Whether the Uri authority is ExternalStorageProvider.
+         */
+        public static boolean isExternalStorageDocument(Uri uri) {
+            return "com.android.externalstorage.documents".equals(uri.getAuthority());
+        }
+
+        /**
+         * @param uri The Uri to check.
+         * @return Whether the Uri authority is DownloadsProvider.
+         */
+        public static boolean isDownloadsDocument(Uri uri) {
+            return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+        }
+
+        /**
+         * @param uri The Uri to check.
+         * @return Whether the Uri authority is MediaProvider.
+         */
+        public static boolean isMediaDocument(Uri uri) {
+            return "com.android.providers.media.documents".equals(uri.getAuthority());
         }
     }
 }
