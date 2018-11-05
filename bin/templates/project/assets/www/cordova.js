725e75fa0dc35a1b9bbc56c8621121e6dbc4fbdf
Darryl Pogue <darryl@dpogue.ca>
2018-11-05 12:17:08 -0800

    Update RELEASENOTES & version for v7.1.2

--- bin/templates/project/assets/www/cordova.js
+++ bin/templates/project/assets/www/cordova.js
@@ -19,7 +19,7 @@
  under the License.
 */
 ;(function() {
-var PLATFORM_VERSION_BUILD_LABEL = '7.1.2-dev';
+var PLATFORM_VERSION_BUILD_LABEL = '7.1.2';
 // file: src/scripts/require.js
 
 /* jshint -W079 */
