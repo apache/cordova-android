f86519b1580d3498c99eeeedff65bdad4da3fe56
Darryl Pogue <darryl@dpogue.ca>
2018-11-05 12:23:23 -0800

    Bump version to 7.1.3-dev

--- bin/templates/project/assets/www/cordova.js
+++ bin/templates/project/assets/www/cordova.js
@@ -19,7 +19,7 @@
  under the License.
 */
 ;(function() {
-var PLATFORM_VERSION_BUILD_LABEL = '7.1.2';
+var PLATFORM_VERSION_BUILD_LABEL = '7.1.3-dev';
 // file: src/scripts/require.js
 
 /* jshint -W079 */
