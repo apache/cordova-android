232d38fe17eab431144f0abe9d8b9b1070722091
Raphael von der Grün <raphinesse@gmail.com>
2019-04-12 21:52:49 +0200

    Allow to pass-through projectPath to Builder

--- bin/templates/cordova/lib/builders/builders.js
+++ bin/templates/cordova/lib/builders/builders.js
@@ -24,10 +24,10 @@ const CordovaError = require('cordova-common').CordovaError;
  *
  * @return {Builder} A builder instance for specified build type.
  */
-module.exports.getBuilder = function () {
+module.exports.getBuilder = function (projectPath) {
     try {
         const Builder = require('./ProjectBuilder');
-        return new Builder();
+        return new Builder(projectPath);
     } catch (err) {
         throw new CordovaError('Failed to instantiate ProjectBuilder builder: ' + err);
     }
