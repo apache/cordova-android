674a7f7e725975d5fa94967ab0b9603e4bc87719
Erisu <ellis.bryan@gmail.com>
2020-01-17 22:15:07 +0900

    feat: add androidx support

--- bin/templates/cordova/lib/config/GradlePropertiesParser.js
+++ bin/templates/cordova/lib/config/GradlePropertiesParser.js
@@ -34,7 +34,11 @@ class GradlePropertiesParser {
             'org.gradle.daemon': 'true',
 
             // to allow dex in process
-            'org.gradle.jvmargs': '-Xmx2048m'
+            'org.gradle.jvmargs': '-Xmx2048m',
+
+            // Android X
+            'android.useAndroidX': 'true',
+            'android.enableJetifier': 'true'
 
             // Shaves another 100ms, but produces a "try at own risk" warning. Not worth it (yet):
             // 'org.gradle.parallel': 'true'
