5d3591b8537647c0cc145308028e537c83e7ffd6
Raphael von der Grün <raphinesse@gmail.com>
2020-10-06 09:04:48 +0200

    refactor(utils): reduce number of utils (#1085)
    * refactor(utils): remove utils.grep
    
    * refactor(utils): replace utils.scanDirectory w/ fast-glob
    
    Note that fast-glob is already in our dependency graph anyway.

--- bin/templates/cordova/lib/utils.js
+++ bin/templates/cordova/lib/utils.js
@@ -24,7 +24,6 @@
 // TODO: Perhaps this should live in cordova-common?
 
 const fs = require('fs-extra');
-const path = require('path');
 
 /**
  * Reads, searches, and replaces the found occurences with replacementString and then writes the file back out.
@@ -39,59 +38,4 @@ exports.replaceFileContents = function (file, searchRegex, replacementString) {
     let contents = fs.readFileSync(file).toString();
     contents = contents.replace(searchRegex, replacementString);
     fs.writeFileSync(file, contents);
-};
-
-/**
- * Reads a file and scans for regex. Returns the line of the first occurence or null if no occurences are found.
- *
- * @param {string} file A file path
- * @param {RegExp} regex A search regex
- * @returns string|null
- */
-exports.grep = function (file, regex) {
-    const contents = fs.readFileSync(file).toString().replace(/\\r/g, '').split('\n');
-    for (let i = 0; i < contents.length; i++) {
-        const line = contents[i];
-        if (regex.test(line)) {
-            return line;
-        }
-    }
-    return null;
-};
-
-/**
- * Scans directories and outputs a list of found paths that matches the regex
- *
- * @param {string} directory The starting directory
- * @param {RegExp} regex The search regex
- * @param {boolean} recursive Enables recursion
- * @returns Array<string>
- */
-exports.scanDirectory = function (directory, regex, recursive) {
-    let output = [];
-
-    if (fs.existsSync(directory)) {
-        const items = fs.readdirSync(directory);
-
-        for (let i = 0; i < items.length; i++) {
-            const item = items[i];
-            const itemPath = path.join(directory, item);
-            const stats = fs.statSync(itemPath);
-
-            if (regex.test(itemPath)) {
-                output.push(itemPath);
-            }
-
-            if (stats.isDirectory()) {
-                if (recursive) {
-                    output = output.concat(exports.scanDirectory(itemPath, regex, recursive));
-                } else {
-                    // Move onto the next item
-                    continue;
-                }
-            }
-        }
-    }
-
-    return output;
 };
