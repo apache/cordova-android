2a92c77772a4d4a9677b6b50fcb46e6297cfdd16
Niklas Merz <niklasmerz@apache.org>
2021-04-13 12:14:37 +0200

    remove app cache settings (#1141)

--- framework/src/org/apache/cordova/engine/SystemWebViewEngine.java
+++ framework/src/org/apache/cordova/engine/SystemWebViewEngine.java
@@ -194,12 +194,6 @@ public class SystemWebViewEngine implements CordovaWebViewEngine {
         // Enable built-in geolocation
         settings.setGeolocationEnabled(true);
 
-        // Enable AppCache
-        // Fix for CB-2282
-        settings.setAppCacheMaxSize(5 * 1048576);
-        settings.setAppCachePath(databasePath);
-        settings.setAppCacheEnabled(true);
-
         // Fix for CB-1405
         // Google issue 4641
         String defaultUserAgent = settings.getUserAgentString();
