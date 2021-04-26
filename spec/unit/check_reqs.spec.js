c283f37a1b2d1ebf87c746f63bb1baaa14b1e27e
David Strauß <david.strauss@ventocom.at>
2021-04-26 13:58:24 +0200

    fix(requirements check): use regex to get java version from javac output

--- spec/unit/check_reqs.spec.js
+++ spec/unit/check_reqs.spec.js
@@ -64,6 +64,19 @@ describe('check_reqs', function () {
 
             await expectAsync(check_reqs.check_java()).toBeResolvedTo({ version: '1.8.0' });
         });
+
+        it('should return the correct version if javac prints _JAVA_OPTIONS', async () => {
+            check_reqs.__set__({
+                java: { getVersion: async () => {
+                    javacOutput = 'Picked up _JAVA_OPTIONS: -Xms1024M -Xmx2048M\njavac 1.8.0_271';
+                    version = /javac\s+([\d.]+)/i.exec(javacOutput)?.[1];
+
+                    return { version };
+                } }
+            });
+
+            await expectAsync(check_reqs.check_java()).toBeResolvedTo({ version: '1.8.0' });
+        });
     });
 
     describe('check_android', function () {
