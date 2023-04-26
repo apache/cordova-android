4182b5a06d1eedd360f96b4be4ad573fdac0470f
Ken Corbett <ken@truepic.com>
2023-04-26 16:13:12 -0400

    Reverting some irrellevant formatting changes

--- framework/src/org/apache/cordova/engine/SystemWebChromeClient.java
+++ framework/src/org/apache/cordova/engine/SystemWebChromeClient.java
@@ -53,10 +53,8 @@ import org.apache.cordova.LOG;
 
 /**
  * This class is the WebChromeClient that implements callbacks for our web view.
- * The kind of callbacks that happen here are on the chrome outside the
- * document,
- * such as onCreateWindow(), onConsoleMessage(), onProgressChanged(), etc.
- * Related
+ * The kind of callbacks that happen here are on the chrome outside the document,
+ * such as onCreateWindow(), onConsoleMessage(), onProgressChanged(), etc. Related
  * to but different than CordovaWebViewClient.
  */
 public class SystemWebChromeClient extends WebChromeClient {
@@ -87,8 +85,7 @@ public class SystemWebChromeClient extends WebChromeClient {
     @Override
     public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
         dialogsHelper.showAlert(message, new CordovaDialogsHelper.Result() {
-            @Override
-            public void gotResult(boolean success, String value) {
+            @Override public void gotResult(boolean success, String value) {
                 if (success) {
                     result.confirm();
                 } else {
@@ -122,15 +119,12 @@ public class SystemWebChromeClient extends WebChromeClient {
      * If the client returns true, WebView will assume that the client will
      * handle the prompt dialog and call the appropriate JsPromptResult method.
      *
-     * Since we are hacking prompts for our own purposes, we should not be using
-     * them for
+     * Since we are hacking prompts for our own purposes, we should not be using them for
      * this purpose, perhaps we should hack console.log to do this instead!
      */
     @Override
-    public boolean onJsPrompt(WebView view, String origin, String message, String defaultValue,
-            final JsPromptResult result) {
-        // Unlike the @JavascriptInterface bridge, this method is always called on the
-        // UI thread.
+    public boolean onJsPrompt(WebView view, String origin, String message, String defaultValue, final JsPromptResult result) {
+        // Unlike the @JavascriptInterface bridge, this method is always called on the UI thread.
         String handledRet = parentEngine.bridge.promptOnJsPrompt(origin, message, defaultValue);
         if (handledRet != null) {
             result.confirm(handledRet);
@@ -155,19 +149,17 @@ public class SystemWebChromeClient extends WebChromeClient {
     @Override
     @SuppressWarnings("deprecation")
     public void onExceededDatabaseQuota(String url, String databaseIdentifier, long currentQuota, long estimatedSize,
-            long totalUsedQuota, WebStorage.QuotaUpdater quotaUpdater) {
-        LOG.d(LOG_TAG, "onExceededDatabaseQuota estimatedSize: %d  currentQuota: %d  totalUsedQuota: %d", estimatedSize,
-                currentQuota, totalUsedQuota);
+            long totalUsedQuota, WebStorage.QuotaUpdater quotaUpdater)
+    {
+        LOG.d(LOG_TAG, "onExceededDatabaseQuota estimatedSize: %d  currentQuota: %d  totalUsedQuota: %d", estimatedSize, currentQuota, totalUsedQuota);
         quotaUpdater.updateQuota(MAX_QUOTA);
     }
 
     @Override
     /**
-     * Instructs the client to show a prompt to ask the user to set the Geolocation
-     * permission state for the specified origin.
+     * Instructs the client to show a prompt to ask the user to set the Geolocation permission state for the specified origin.
      *
-     * This also checks for the Geolocation Plugin and requests permission from the
-     * application to use Geolocation.
+     * This also checks for the Geolocation Plugin and requests permission from the application  to use Geolocation.
      *
      * @param origin
      * @param callback
@@ -175,15 +167,15 @@ public class SystemWebChromeClient extends WebChromeClient {
     public void onGeolocationPermissionsShowPrompt(String origin, Callback callback) {
         super.onGeolocationPermissionsShowPrompt(origin, callback);
         callback.invoke(origin, true, false);
-        // Get the plugin, it should be loaded
+        //Get the plugin, it should be loaded
         CordovaPlugin geolocation = parentEngine.pluginManager.getPlugin("Geolocation");
-        if (geolocation != null && !geolocation.hasPermisssion()) {
+        if(geolocation != null && !geolocation.hasPermisssion())
+        {
             geolocation.requestPermissions(0);
         }
     }
 
-    // API level 7 is required for this, see if we could lower this using something
-    // else
+    // API level 7 is required for this, see if we could lower this using something else
     @Override
     @SuppressWarnings("deprecation")
     public void onShowCustomView(View view, WebChromeClient.CustomViewCallback callback) {
@@ -200,7 +192,6 @@ public class SystemWebChromeClient extends WebChromeClient {
     /**
      * Ask the host application for a custom progress view to show while
      * a <video> is loading.
-     * 
      * @return View The progress view.
      */
     public View getVideoLoadingProgressView() {
@@ -210,14 +201,12 @@ public class SystemWebChromeClient extends WebChromeClient {
             // create the linear layout
             LinearLayout layout = new LinearLayout(parentEngine.getView().getContext());
             layout.setOrientation(LinearLayout.VERTICAL);
-            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
-                    LayoutParams.WRAP_CONTENT);
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
             layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
             layout.setLayoutParams(layoutParams);
             // the proress bar
             ProgressBar bar = new ProgressBar(parentEngine.getView().getContext());
-            LinearLayout.LayoutParams barLayoutParams = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
-                    LayoutParams.WRAP_CONTENT);
+            LinearLayout.LayoutParams barLayoutParams = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
             barLayoutParams.gravity = Gravity.CENTER;
             bar.setLayoutParams(barLayoutParams);
             layout.addView(bar);
@@ -363,7 +352,7 @@ public class SystemWebChromeClient extends WebChromeClient {
         request.grant(request.getResources());
     }
 
-    public void destroyLastDialog() {
+    public void destroyLastDialog(){
         dialogsHelper.destroyLastDialog();
     }
 }
