2a1b5c66b075ad16cbd43157d6cde43f386a2e9c
Norman Breau <norman@breautek.com>
2024-05-09 12:12:16 -0300

    fix(test): normalise gradle paths

--- spec/unit/builders/ProjectBuilder.spec.js
+++ spec/unit/builders/ProjectBuilder.spec.js
@@ -136,14 +136,14 @@ describe('ProjectBuilder', () => {
 
         it('should run gradle wrapper 8.7', async () => {
             await builder.installGradleWrapper('8.7');
-            expect(execaSpy).toHaveBeenCalledWith('/root/gradlew', ['-p', '/root', 'wrapper', '--gradle-version', '8.7', '--validate-url'], jasmine.any(Object));
+            expect(execaSpy).toHaveBeenCalledWith(path.normalize('/root/gradlew'), ['-p', '/root', 'wrapper', '--gradle-version', '8.7', '--validate-url'], jasmine.any(Object));
         });
 
         it('CORDOVA_ANDROID_GRADLE_DISTRIBUTION_URL should override gradle version', async () => {
             process.env.CORDOVA_ANDROID_GRADLE_DISTRIBUTION_URL = 'https://dist.local';
             await builder.installGradleWrapper('8.7');
             delete process.env.CORDOVA_ANDROID_GRADLE_DISTRIBUTION_URL;
-            expect(execaSpy).toHaveBeenCalledWith('/root/gradlew', ['-p', '/root', 'wrapper', '--gradle-distribution-url', 'https://dist.local', '--validate-url'], jasmine.any(Object));
+            expect(execaSpy).toHaveBeenCalledWith(path.normalize('/root/gradlew'), ['-p', '/root', 'wrapper', '--gradle-distribution-url', 'https://dist.local', '--validate-url'], jasmine.any(Object));
         });
 
         it('should error if attempting to install an unacceptable gradle version', async () => {
