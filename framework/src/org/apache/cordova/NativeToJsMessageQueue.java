9a2142657d3cae6dedd529b33e5d3abe5068bab5
Erisu <erisu@apache.org>
2024-11-13 10:24:04 +0900

    fix(GH-1735): remove pollOnce from NativeToJsMessageQueue.popAndEncodeAsJs

--- framework/src/org/apache/cordova/NativeToJsMessageQueue.java
+++ framework/src/org/apache/cordova/NativeToJsMessageQueue.java
@@ -205,9 +205,6 @@ public class NativeToJsMessageQueue {
                     sb.append("}finally{");
                 }
             }
-            if (!willSendAllMessages) {
-                sb.append("window.setTimeout(function(){cordova.require('cordova/plugin/android/polling').pollOnce();},0);");
-            }
             for (int i = willSendAllMessages ? 1 : 0; i < numMessagesToSend; ++i) {
                 sb.append('}');
             }
