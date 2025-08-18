3044b94e1526ceb33374da750b13544952e8312b
Erisu <erisu@apache.org>
2025-08-18 15:55:16 +0900

    chore: update default package id

--- spec/unit/create.spec.js
+++ spec/unit/create.spec.js
@@ -155,10 +155,10 @@ describe('create', function () {
         });
 
         describe('parameter values and defaults', function () {
-            it('should have a default package name of io.cordova.helloCordova', () => {
+            it('should have a default package name of org.apache.cordova.hellocordova', () => {
                 config_mock.packageName.and.returnValue(undefined);
                 return create.create(project_path, config_mock, {}, events_mock).then(() => {
-                    expect(create.validatePackageName).toHaveBeenCalledWith('io.cordova.helloCordova');
+                    expect(create.validatePackageName).toHaveBeenCalledWith('org.apache.cordova.hellocordova');
                 });
             });
 
