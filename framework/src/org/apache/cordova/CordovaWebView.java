5cc0a8299eedcf5379b27cd2f32f3f1838a6920a
エリス <erisu@users.noreply.github.com>
2026-07-22 16:44:28 +0900

    chore(post-release): bump v15.1.1-dev.0 (#1981)

--- framework/src/org/apache/cordova/CordovaWebView.java
+++ framework/src/org/apache/cordova/CordovaWebView.java
@@ -33,7 +33,7 @@ import android.webkit.WebChromeClient.CustomViewCallback;
  * are not expected to implement it.
  */
 public interface CordovaWebView {
-    public static final String CORDOVA_VERSION = "15.1.0";
+    public static final String CORDOVA_VERSION = "15.1.1-dev.0";
 
     void init(CordovaInterface cordova, List<PluginEntry> pluginEntries, CordovaPreferences preferences);
 
